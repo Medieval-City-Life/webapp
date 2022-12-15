32858c8cae69e6fc2f990db51a669e1ca5dc10b2
Oliver Köhler <olli9622@gmail.com>
2022-12-15 18:02:47 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="text-brown bg-primary h-screen pt-10">
+    <AppLoginForm v-if="!loggedIn" />
+    <AppDashboard v-else />
+    <AppError v-if="message"/>
+  </div>
+</template>
+
+<script setup>
+import AppLoginForm from './components/AppLoginForm.vue';
+import AppDashboard from './components/AppDashboard.vue';
+import AppError from './components/AppError.vue';
+import { storeToRefs } from 'pinia';
+import { useUserStore } from './stores/user';
+import { useErrorsStore } from './stores/useErrors';
+import './assets/tailwind.css';
+
+const { loggedIn } = storeToRefs(useUserStore());
+const { message, code } = storeToRefs(useErrorsStore());
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/AppDashboard.vue
@@ -0,0 +1,21 @@
+<template lang="">
+  <div>
+    <div class="text-2xl font-bold capitalize">Willkommen {{ user.name }}!</div>
+    <div class="text-xs">ID: {{ user._id }}</div>
+    <AppButton @click="handleLogoutClick">Ausloggen</AppButton>
+  </div>
+</template>
+
+<script setup>
+import { useUserStore } from '../stores/user';
+import { storeToRefs } from 'pinia';
+
+import AppButton from './AppButton.vue';
+
+const { user } = storeToRefs(useUserStore());
+const { logout } = useUserStore();
+
+const handleLogoutClick = () => {
+  logout();
+};
+</script>
