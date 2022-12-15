241cc5ad5df2b81ae320fa20197465de4720640e
Oliver Köhler <olli9622@gmail.com>
2022-12-15 23:39:49 +0100

    revert

--- src/plugins/apollo.js
+++ src/plugins/apollo.js
@@ -7,17 +7,8 @@ import { onError } from "@apollo/client/link/error";
 import { useErrorsStore } from "../stores/useErrors";
 import { setContext } from "@apollo/client/link/context";
 
-let apiURL;
-
-const urlOrigin = window.location.origin;
-
-if (urlOrigin.includes('localhost'))
-  apiURL = 'http://localhost:4000/graphql';
-else if (urlOrigin.includes('trade-of-kings'))
-  apiURL = 'https://54.93.43.110:4000/graphql';
-
 const httpLink = createHttpLink({
-  uri: apiURL,
+  uri: "http://localhost:4000/graphql",
 });
 
 const errorHandler = onError(({ graphQLErrors }) => {
